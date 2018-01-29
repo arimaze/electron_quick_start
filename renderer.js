1e0f129d49d0de7599e85c96df81725442604a56
arimaze <adrian.foata@gmail.com>
2018-01-29 15:43:49 +0100

    list directory

--- renderer.js
+++ renderer.js
@@ -9,90 +9,103 @@ var respond = require('respond');
 var XLSX = require('xlsx');
 
 var fs = require('fs');
+const path = require('path');
 var dialog = require('electron').remote.dialog;
 
 
-/* show a file-open dialog and read the first selected file */
-// var o = dialog.showOpenDialog({ properties: ['openFile'] });
-// var workbook = XLSX.readFile(o[0]);
-var workbook = XLSX.readFile('./metadata.xlsx');
-var ws = workbook.SheetNames[0];
-//console.log(ws);
-var address_of_cell = 'A1';
-var desired_cell = ws[address_of_cell];
-//console.log(desired_cell);
-var desired_value = (desired_cell ? desired_cell.v : undefined);
 
-var sheet_name_list = workbook.SheetNames;
-var xlData = XLSX.utils.sheet_to_html(workbook.Sheets[sheet_name_list[0]]);
+//var workbook = XLSX.readFile('./metadata.xlsx');
+//var worksheet = workbook.Sheets[sheet_name_list[0]];
+//var xlData = XLSX.utils.sheet_to_json(worksheet);
 //console.log(xlData);
-//var container = document.getElementById('tableau');
-//container.innerHTML=xlData;
+// xlData.forEach(function(table,index) {
+// if(index>=1)
+// {
+// //  console.log(xlData[index -1].Name);
+//     var section = table["SMSSection"];
+//     if(xlData[index - 1]["SMSSection"] == section)
+//     {
+//       var documentName = table["Name"];
+//       $("<div>"+documentName+"</div>").appendTo("footer");
+//     }
+//     else
+//     {
+//     $("<h2>"+section+"</h2>").appendTo("footer");
+//     //console.log(section);
+//     }
+//       var documentName = table["Name"];
+//      $("<div>"+documentName+"</div>").appendTo("footer");
+//
+//   }
+//   else{
+//       var section = table["SMSSection"];
+//       var documentName = table["Name"];
+//       $("<h2>"+section+"</h2>").appendTo("footer");
+//       $("<div>"+documentName+"</div>").appendTo("footer");
+//   }
+//   }
+//   );
 
-var worksheet = workbook.Sheets[sheet_name_list[0]];
-var xlData = XLSX.utils.sheet_to_json(worksheet);
-//console.log(xlData);
-xlData.forEach(function(table,index) {
-if(index>=1)
-{
-  console.log(xlData[index -1].Name);
-    var section = table["SMSSection"];
-    if(xlData[index - 1]["SMSSection"] == section)
-    {
-      var documentName = table["Name"];
-      $("<div>"+documentName+"</div>").appendTo("footer");
+
+
+function filewalker(dir, done) {
+    let results = [];
+
+    fs.readdir(dir, function(err, list) {
+        if (err) return done(err);
+
+        var pending = list.length;
+
+        if (!pending) return done(null, results);
+
+        list.forEach(function(file,index){
+           var itemName = file;
+
+            file = path.resolve(dir, file);
+
+            fs.stat(file, function(err, stat){
+                // If directory, execute a recursive call
+                if (stat && stat.isDirectory()) {
+                    // Add directory to array [comment if you need to remove the directories from the array]
+                  // results.push(file);
+          //  $('<h3>'+itemName+"</h3>").appendTo("footer");
+                    filewalker(file, function(err, res){
+
+                        results = results.concat(res);
+                        if (!--pending) done(null, results);
+                    });
+                }
+                else {
+                    results.push(file);
+                    if(index>=1){
+                    var previous = path.resolve(dir,list[index -1]);
+
+                    //console.log(previous);
+if(path.basename(path.dirname(file)) == path.basename(path.dirname(previous)) ){
+     $("<div>"+itemName+"</div>").appendTo("footer");
+}
+else {
+  $("<h3>"+path.basename(path.dirname(file))+"</h3>").appendTo("footer");
+        $("<div>"+itemName+"</div>").appendTo("footer");
+}
+}
+else {
+  $("<h3>"+path.basename(path.dirname(file))+"</h3>").appendTo("footer");
+     $("<div>"+itemName+"</div>").appendTo("footer");
+}
+
+                if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+};
+
+filewalker("c:/opentext", function(err, data){
+    if(err){
+        throw err;
     }
-    else
-    {
-    $("<h2>"+section+"</h2>").appendTo("footer");
-    console.log(section);
-    }
-      var documentName = table["Name"];
-     $("<div>"+documentName+"</div>").appendTo("footer");
-
-  }
-  else{
-      var section = table["SMSSection"];
-      var documentName = table["Name"];
-      $("<h2>"+section+"</h2>").appendTo("footer");
-      $("<div>"+documentName+"</div>").appendTo("footer");
-  }
-  }
-  );
-
-
-//console.log(xlData);
-// var headers = {};
-//    var data = [];
-//
-// for(z in worksheet) {
-//        if(z[0] === '!') continue;
-//        //parse out the column, row, and value
-//        var tt = 0;
-//        for (var i = 0; i < z.length; i++) {
-//            if (!isNaN(z[i])) {
-//                tt = i;
-//                break;
-//            }
-//        };
-//        var col = z.substring(0,tt);
-//        var row = parseInt(z.substring(tt));
-//        var value = worksheet[z].v;
-//
-//        //store header names
-//        if(row == 1 && value) {
-//            headers[col] = value;
-//            continue;
-//        }
-//
-//        if(!data[row]) data[row]={};
-//        data[row][headers[col]] = value;
-//    }
-//    data.shift();
-//    data.shift();
-//    console.log(data);
-
-
-var mm = xlData.Name;
-//$("<div>"+mm+"</div>").appendTo("footer");
-//console.log($("body").html());
+    // ["c://some-existent-path/file.txt","c:/some-existent-path/subfolder"]
+  //  console.log(data);
+     $("<div>test</div>").appendTo("footer");
+});
